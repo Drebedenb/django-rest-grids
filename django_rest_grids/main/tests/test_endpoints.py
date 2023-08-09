94b9b2cbccc9eb889ce96fc2a27cd8a886a22b5b
Drebedenb <danila.kuzya2001@gmail.com>
2023-08-09 09:55:44 +0300

    Add test and CI/CD pipeline

---
+++ django_rest_grids/main/tests/test_endpoints.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+
+
+class UnitTestOfEndpoints(SimpleTestCase):
+    def test_valid_count(self):
+        url = '/count'
+        response = self.client.get(url)
+        self.assertEqual(response.data['count']['count'], 686) #TODO: how to make in more beaty way?
+
+    def test_valid_min_price(self):
+        url = '/min_price'
+        response = self.client.get(url)
+        self.assertEqual(response.data['min_price']['min_price'], 1270) #TODO: how to make in more beaty way?
+
+    def test_valid_max_price(self):
+        url = '/max_price'
+        response = self.client.get(url)
+        self.assertEqual(response.data['max_price']['max_price'], 686) #TODO: how to make in more beaty way?
+
+
